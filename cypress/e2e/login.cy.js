c0206b55ead254310ce0499f9d8f318eb922019e
Edson José dos Santos <edsonj82@outlook.com>
2024-02-13 01:37:51 -0300

    (login) validating login field (done)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,4 +13,47 @@ describe('login', () => {
                 expect(element).not.disabled
             })
     })
+
+    it('validate login field', () => {
+        cy.visit('/')
+            .get('.header-logo')
+
+        cy.get('.fa-user')
+            .click() //doubleClick //rightClick
+
+        cy.get('.account_form h3')
+            .should('contain', 'Login')
+            .should('have.text', 'Login')
+
+        cy.get('.default-form-box label')
+            .should('contain', 'E-mail')
+            .should('be.visible')
+
+        cy.get('#user')
+            .then((element) => {
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
+
+        cy.get('.default-form-box label')
+            .should('contain', 'Senha')
+            .should('be.visible')
+
+        cy.get('#password')
+            .then((element) => {
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
+
+        //btnLogin
+        cy.get('#btnLogin')
+            .then((element) => {
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
+
+
+
+
+    })
 })
