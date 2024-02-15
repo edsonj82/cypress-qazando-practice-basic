71f0b91b7a6aacb4ab0b63c72f7764879107becf
Edson José dos Santos <edsonj82@outlook.com>
2024-02-15 17:26:04 -0300

    Validating access register screen

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,7 +1,15 @@
 ///<reference types="cypress"/>
 
 describe("register", ()=>{
-    it('access login screen', () => {
+    it.only('access register screen', () => {
         
+        cy.visit('/').get('.header-logo')
+        cy.get('.fa-lock').click()
+        cy.get('.account_form h3')
+            .then((element) => {
+                expect(element.text()).eq('Cadastro de usuário')
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
     });
 })
