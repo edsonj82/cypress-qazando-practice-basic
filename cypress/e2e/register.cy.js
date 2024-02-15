b9d4b6d08be7ea3cc60c6c9b7c42e4ef8b4f9e71
Edson José dos Santos <edsonj82@outlook.com>
2024-02-15 18:05:01 -0300

    Validating register field

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,13 +1,45 @@
 ///<reference types="cypress"/>
 
-describe("register", ()=>{
+describe("register", () => {
     it('access register screen', () => {
-        
+
         cy.visit('/').get('.header-logo')
         cy.get('.fa-lock').click()
         cy.get('.account_form h3')
             .then((element) => {
                 expect(element.text()).eq('Cadastro de usuário')
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
+    });
+
+    it('validate register field', () => {
+        cy.visit('/').get('.header-logo')
+        cy.get('.fa-lock').click()
+        cy.get('.account_form h3')
+            .should('contain', 'Cadastro de usuário')
+            .should('have.text', 'Cadastro de usuário')
+        cy.get('.default-form-box label')
+            .should('contain', 'Nome')
+            .should('be.visible')
+        cy.get('#user')
+            .then((element) => {
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
+        cy.get('.default-form-box label')
+            .should('contain', 'E-mail')
+            .should('be.visible')
+        cy.get('#email')
+            .then((element) => {
+                expect(element).to.be.visible
+                expect(element).not.disabled
+            })
+        cy.get('.default-form-box label')
+            .should('contain', 'Senha')
+            .should('be.visible')
+        cy.get('#password')
+            .then((element) => {
                 expect(element).to.be.visible
                 expect(element).not.disabled
             })
