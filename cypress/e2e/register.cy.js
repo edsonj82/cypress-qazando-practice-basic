1c60c9f0ce35518fd35e8e85b113d27608b8827c
Edson José dos Santos <edsonj82@outlook.com>
2024-02-15 18:12:22 -0300

    Validating blank name field in register.cy

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,6 +1,10 @@
 ///<reference types="cypress"/>
 
 describe("register", () => {
+    const user_name = 'Eduardo Finotti'
+    const user_email = 'eduardo.finotti@qazando.com'
+    const user_password = '123456'
+
     it('access register screen', () => {
 
         cy.visit('/').get('.header-logo')
@@ -44,4 +48,15 @@ describe("register", () => {
                 expect(element).not.disabled
             })
     });
+
+    it('blank name field', () => {
+        cy.visit('/').get('.header-logo')
+        cy.get('.fa-lock').click() //doubleClick //rightClick
+        cy.get('#email').type(user_email)
+        cy.get('#password').type(user_password)
+        cy.get('#btnRegister').click()
+        cy.get('#errorMessageFirstName')
+            .should('be.visible')
+            .should('have.text', 'O campo nome deve ser prenchido')
+    })
 })
