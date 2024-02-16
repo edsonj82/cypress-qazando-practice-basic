2ca42bb9f9e901b7ef8ab517db701f6e428bfe1d
Edson José dos Santos <edsonj82@outlook.com>
2024-02-16 11:15:06 -0300

    Using fakeJs library in register.cy (wip)

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,4 +1,5 @@
 ///<reference types="cypress"/>
+import { faker } from '@faker-js/faker';
 
 const user_data = require('../fixtures/register_user_create.json')
 
@@ -10,7 +11,7 @@ describe("register", () => {
     it('should register user in successfully', () => {
         cy.visit('/').get('.header-logo')
         cy.get('.fa-lock').click() //doubleClick //rightClick
-        cy.get('#user').type(user_data.user_name)
+        cy.get('#user').type(faker.person.fullName())//.type(user_data.user_name)
         cy.get('#email').type(user_data.user_email)
         cy.get('#password').type(user_data.user_password)
         cy.get('#btnRegister').click()
@@ -122,10 +123,13 @@ describe("register", () => {
     })
 
     it('validating register modal successfully', () => {
+        const name = faker.person.fullName()
+        const email = faker.internet.email()
+        
         cy.visit('/').get('.header-logo')
         cy.get('.fa-lock').click() //doubleClick //rightClick
-        cy.get('#user').type(user_data.user_name)
-        cy.get('#email').type(user_data.user_email)
+        cy.get('#user').type(name)
+        cy.get('#email').type(email)
         cy.get('#password').type(user_data.user_password)
         cy.get('#btnRegister').click()
         cy.get('#swal2-title')
@@ -133,7 +137,7 @@ describe("register", () => {
             .should('have.text', 'Cadastro realizado!')
             cy.get('#swal2-html-container')
             .should('be.visible')
-            .should('have.text', `Bem-vindo ${user_data.user_name}`)
+            .should('have.text', `Bem-vindo ${name}`)
         cy.get('.swal2-confirm')
             .should('be.visible')
             .should('have.text', 'OK')
