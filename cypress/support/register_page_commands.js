448a31b455a499b3416a4ba8f5eb6fbc86d00a48
Edson José dos Santos <edsonj82@outlook.com>
2024-02-21 10:06:34 -0300

    separating elements button, fields and messages (objects)

--- cypress/support/register_page_commands.js
+++ cypress/support/register_page_commands.js
@@ -1,32 +1,50 @@
 ///<reference types="cypress"/>
 
+
+///Elements
+const elements = {
+    buttons: {
+        btnRegister: '#btnRegister'
+    },
+    fields: {
+        name: '#user',
+        email: '#email',
+        password: '#password'
+    },
+    messages: {
+        error: '#errorMessageFirstName',
+        successTitle: '#swal2-title',
+        successSubTitle: '#swal2-html-container'
+    }
+}
+///Actions / Methods / Functions
 Cypress.Commands.add('saveRegister', () => {
-    cy.get('#btnRegister').click()
+    cy.get(elements.buttons.btnRegister).click()
 })
 
 Cypress.Commands.add('fillEmail', (email) => {
-    cy.get('#email').type(email)
+    cy.get(elements.fields.email).type(email)
 })
 
 Cypress.Commands.add('fillName', (name) => {
-    cy.get('#user').type(name)
+    cy.get(elements.fields.name).type(name)
 })
 
 Cypress.Commands.add('fillPassword', (password) => {
-    cy.get('#password').type(password)
+    cy.get(elements.fields.password).type(password)
 })
 
 Cypress.Commands.add('checkMessage', (message) => {
-    cy.get('#errorMessageFirstName')
+    cy.get(elements.messages.error)
         .should('be.visible')
         .should('have.text', message)
 })
 
 Cypress.Commands.add('checkModalMessage', (modalMessage) => {
-    cy.get('#swal2-title')
+    cy.get(elements.messages.successTitle)
         .should('be.visible')
         .should('have.text', 'Cadastro realizado!')
-    cy.get('#swal2-html-container')
+    cy.get(elements.messages.successSubTitle)
         .should('be.visible')
         .should('have.text', `Bem-vindo ${modalMessage}`)
 })
