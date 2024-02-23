f38b29fd8d0e92000e2d2e9f572f3c9f30edc9d6
Edson José dos Santos <edsonj82@outlook.com>
2024-02-23 15:44:47 -0300

    simulating for desktop, mobile and tablet screens (login & register page)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,99 +2,107 @@
 const user_data = require('../fixtures/login_user_create.json')
 import login_page from '../support/pages/login_page'
 
-describe('login', () => {
-    beforeEach('accessing the login page', () => {
-        login_page.accessLoginPage()
+const screens = ['desktop', 'iphone-x', 'ipad-mini']
+screens.forEach(element => {
+
+
+    describe(`login - ${element}`, () => {
+        beforeEach('accessing the login page', () => {
+            if (element != 'desktop')
+                cy.viewport(element)
+
+            login_page.accessLoginPage()
+        })
+
+        it('should log in successfully', () => {
+            login_page.fillEmailLogin(user_data.user_email)
+            login_page.fillPasswordLogin(user_data.user_password)
+            login_page.loginUser()
+            cy.get('#swal2-title')
+                .should('be.visible')
+                .should('have.text', 'Login realizado')
+            cy.get('.swal2-confirm').click()
+        });
+
+        it('access login screen', () => {
+            cy.get('.account_form h3')
+                .then((element) => {
+                    expect(element.text()).eq('Login')
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
+        })
+
+        it('validate login field', () => {
+            cy.get('.account_form h3')
+                .should('contain', 'Login')
+                .should('have.text', 'Login')
+            cy.get('.default-form-box label')
+                .should('contain', 'E-mail')
+                .should('be.visible')
+            cy.get('#user')
+                .then((element) => {
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
+            cy.get('.default-form-box label')
+                .should('contain', 'Senha')
+                .should('be.visible')
+            cy.get('#password')
+                .then((element) => {
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
+            cy.get('#btnLogin')
+                .then((element) => {
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
+        })
+
+        it('blank username field', () => {
+            login_page.fillPasswordLogin(user_data.user_password)
+            cy.get('#btnLogin').click()
+            cy.get('.invalid_input')
+                .should('be.visible')
+                .should('have.text', 'E-mail inválido.')
+        })
+
+        it('blank password field', () => {
+            login_page.fillEmailLogin(user_data.user_email)
+            login_page.loginUser()
+            cy.get('.invalid_input')
+                .should('be.visible')
+                .should('have.text', 'Senha inválida.')
+        })
+
+        // --required password field
+        it('invalid e-mail', () => {
+            cy.get('#user').type('teste@123')
+            login_page.loginUser()
+            cy.get('.invalid_input')
+                .should('be.visible')
+                .should('have.text', 'E-mail inválido.')
+        });
+
+        it('invalid password', () => {
+            login_page.fillEmailLogin(user_data.user_email)
+            cy.get('#password').type('12345')
+            login_page.loginUser()
+            cy.get('.invalid_input')
+                .should('be.visible')
+                .should('have.text', 'Senha inválida.')
+        });
+
+        it('validating login modal successfully', () => {
+            login_page.fillEmailLogin(user_data.user_email)
+            login_page.fillPasswordLogin(user_data.user_password)
+            login_page.loginUser()
+            login_page.checkModalMessageLogin(user_data.user_email)
+            cy.get('.swal2-confirm')
+                .should('be.visible')
+                .should('have.text', 'OK')
+                .click()
+        });
     })
-
-    it('should log in successfully', () => {
-        login_page.fillEmailLogin(user_data.user_email)
-        login_page.fillPasswordLogin(user_data.user_password)
-        login_page.loginUser()
-        cy.get('#swal2-title')
-            .should('be.visible')
-            .should('have.text', 'Login realizado')
-        cy.get('.swal2-confirm').click()
-    });
-
-    it('access login screen', () => {
-        cy.get('.account_form h3')
-            .then((element) => {
-                expect(element.text()).eq('Login')
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
-    })
-
-    it('validate login field', () => {
-        cy.get('.account_form h3')
-            .should('contain', 'Login')
-            .should('have.text', 'Login')
-        cy.get('.default-form-box label')
-            .should('contain', 'E-mail')
-            .should('be.visible')
-        cy.get('#user')
-            .then((element) => {
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
-        cy.get('.default-form-box label')
-            .should('contain', 'Senha')
-            .should('be.visible')
-        cy.get('#password')
-            .then((element) => {
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
-        cy.get('#btnLogin')
-            .then((element) => {
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
-    })
-
-    it('blank username field', () => {
-        login_page.fillPasswordLogin(user_data.user_password)
-        cy.get('#btnLogin').click()
-        cy.get('.invalid_input')
-            .should('be.visible')
-            .should('have.text', 'E-mail inválido.')
-    })
-
-    it('blank password field', () => {
-        login_page.fillEmailLogin(user_data.user_email)
-        login_page.loginUser()
-        cy.get('.invalid_input')
-            .should('be.visible')
-            .should('have.text', 'Senha inválida.')
-    })
-
-    // --required password field
-    it('invalid e-mail', () => {
-        cy.get('#user').type('teste@123')
-        login_page.loginUser()
-        cy.get('.invalid_input')
-            .should('be.visible')
-            .should('have.text', 'E-mail inválido.')
-    });
-
-    it('invalid password', () => {
-        login_page.fillEmailLogin(user_data.user_email)
-        cy.get('#password').type('12345')
-        login_page.loginUser()
-        cy.get('.invalid_input')
-            .should('be.visible')
-            .should('have.text', 'Senha inválida.')
-    });
-
-    it('validating login modal successfully', () => {
-        login_page.fillEmailLogin(user_data.user_email)
-        login_page.fillPasswordLogin(user_data.user_password)
-        login_page.loginUser()
-        login_page.checkModalMessageLogin(user_data.user_email)
-        cy.get('.swal2-confirm')
-            .should('be.visible')
-            .should('have.text', 'OK')
-            .click()
-    });
 })
--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -5,177 +5,184 @@ import register_page from '../support/pages/register_page';
 
 const user_data = require('../fixtures/register_user_create.json')
 
-describe("register", () => {
-    // const user_name = 'Eduardo Finotti'
-    // const user_email = 'eduardo.finotti@qazando.com'
-    // const user_password = '123456'
-    beforeEach('accessing the registration page', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.accessRegisterPage()
-        home_page.accessRegisterPage()
-    })
+const screens = ['desktop', 'iphone-x', 'ipad-mini']
+screens.forEach(element => {
 
-    it('should register user in successfully', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#user').type(faker.person.fullName())//.type(user_data.user_name)
-        register_page.fillName(user_data.user_name)
-        // cy.get('#email').type(user_data.user_email)
-        register_page.fillEmail(user_data.user_email)
-        // cy.get('#password').type(user_data.user_password)
-        register_page.fillPassword(user_data.user_password)
+    describe(`register - ${element}`, () => {
+        // const user_name = 'Eduardo Finotti'
+        // const user_email = 'eduardo.finotti@qazando.com'
+        // const user_password = '123456'
+        beforeEach('accessing the registration page', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.accessRegisterPage()
+            if (element != 'desktop')
+                cy.viewport(element)
 
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        cy.get('#swal2-title')
-            .should('be.visible')
-            .should('have.text', 'Cadastro realizado!')
+            home_page.accessRegisterPage()
+        })
+
+        it('should register user in successfully', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#user').type(faker.person.fullName())//.type(user_data.user_name)
+            register_page.fillName(user_data.user_name)
+            // cy.get('#email').type(user_data.user_email)
+            register_page.fillEmail(user_data.user_email)
+            // cy.get('#password').type(user_data.user_password)
+            register_page.fillPassword(user_data.user_password)
+
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            cy.get('#swal2-title')
+                .should('be.visible')
+                .should('have.text', 'Cadastro realizado!')
             cy.get('.swal2-confirm').click()
-    });
+        });
 
-    it('access register screen', () => {
+        it('access register screen', () => {
 
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click()
-        cy.get('.account_form h3')
-            .then((element) => {
-                expect(element.text()).eq('Cadastro de usuário')
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
-    });
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click()
+            cy.get('.account_form h3')
+                .then((element) => {
+                    expect(element.text()).eq('Cadastro de usuário')
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
+        });
 
-    it('validate register field', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click()
-        cy.get('.account_form h3')
-            .should('contain', 'Cadastro de usuário')
-            .should('have.text', 'Cadastro de usuário')
+        it('validate register field', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click()
+            cy.get('.account_form h3')
+                .should('contain', 'Cadastro de usuário')
+                .should('have.text', 'Cadastro de usuário')
             cy.get('.default-form-box label')
-            .should('contain', 'Nome')
-            .should('be.visible')
+                .should('contain', 'Nome')
+                .should('be.visible')
             cy.get('#user')
-            .then((element) => {
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
+                .then((element) => {
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
             cy.get('.default-form-box label')
-            .should('contain', 'E-mail')
-            .should('be.visible')
+                .should('contain', 'E-mail')
+                .should('be.visible')
             cy.get('#email')
-            .then((element) => {
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
+                .then((element) => {
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
             cy.get('.default-form-box label')
-            .should('contain', 'Senha')
-            .should('be.visible')
+                .should('contain', 'Senha')
+                .should('be.visible')
             cy.get('#password')
-            .then((element) => {
-                expect(element).to.be.visible
-                expect(element).not.disabled
-            })
-    });
+                .then((element) => {
+                    expect(element).to.be.visible
+                    expect(element).not.disabled
+                })
+        });
 
-    it('blank name field', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#email').type(user_data.user_email)
-        register_page.fillEmail(user_data.user_email)
-        // cy.get('#password').type(user_data.user_password)
-        register_page.fillPassword(user_data.user_password)
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo nome deve ser prenchido')
+        it('blank name field', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#email').type(user_data.user_email)
+            register_page.fillEmail(user_data.user_email)
+            // cy.get('#password').type(user_data.user_password)
+            register_page.fillPassword(user_data.user_password)
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            cy.get('#errorMessageFirstName')
+                .should('be.visible')
+                .should('have.text', 'O campo nome deve ser prenchido')
+        })
+
+        it('blank e-mail field', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#user').type(user_data.user_name)
+            register_page.fillName(user_data.user_name)
+            // cy.get('#password').type(user_data.user_password)
+            register_page.fillPassword(user_data.user_password)
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            cy.get('#errorMessageFirstName')
+                .should('be.visible')
+                .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
+        })
+
+        it('blank password field', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#user').type(user_data.user_name)
+            register_page.fillName(user_data.user_name)
+            // cy.get('#email').type(user_data.user_email)
+            register_page.fillEmail(user_data.user_email)
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            cy.get('#errorMessageFirstName')
+                .should('be.visible')
+                .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
+        })
+
+        it('invalid e-mail', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#user').type(user_data.user_name)
+            register_page.fillName(user_data.user_name)
+            cy.get('#email').type('username@teste')
+            // cy.get('#password').type(user_data.user_password)
+            register_page.fillPassword(user_data.user_password)
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            // cy.get('#errorMessageFirstName')
+            //     .should('be.visible')
+            //     .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
+            register_page.checkMessage('O campo e-mail deve ser prenchido corretamente')
+        })
+
+        it('blank password field', () => {
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#user').type(user_data.user_name)
+            register_page.fillName(user_data.user_name)
+            // cy.get('#email').type(user_data.user_email)
+            register_page.fillEmail(user_data.user_email)
+            cy.get('#password').type('pass@')
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            // cy.get('#errorMessageFirstName')
+            //     .should('be.visible')
+            //     .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
+            register_page.checkMessage('O campo senha deve ter pelo menos 6 dígitos')
+        })
+
+        it('validating register modal successfully', () => {
+            // const name = faker.person.fullName()
+            // const email = faker.internet.email()
+
+            // cy.visit('/').get('.header-logo')
+            // cy.get('.fa-lock').click() //doubleClick //rightClick
+            // cy.get('#user').type(name)
+            register_page.fillName(user_data.user_name)
+            // cy.get('#email').type(email)
+            register_page.fillEmail(user_data.user_email)
+            // cy.get('#password').type(user_data.user_password)
+            register_page.fillPassword(user_data.user_password)
+            // cy.get('#btnRegister').click()
+            register_page.saveRegister()
+            // cy.get('#swal2-title')
+            //     .should('be.visible')
+            //     .should('have.text', 'Cadastro realizado!')
+            // cy.get('#swal2-html-container')
+            //     .should('be.visible')
+            //     .should('have.text', `Bem-vindo ${user_data.user_name}`)
+            register_page.checkModalMessage(user_data.user_name)
+            cy.get('.swal2-confirm')
+                .should('be.visible')
+                .should('have.text', 'OK')
+                .click()
+        });
     })
-
-    it('blank e-mail field', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#user').type(user_data.user_name)
-        register_page.fillName(user_data.user_name)
-        // cy.get('#password').type(user_data.user_password)
-        register_page.fillPassword(user_data.user_password)
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
-    })
-
-    it('blank password field', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#user').type(user_data.user_name)
-        register_page.fillName(user_data.user_name)
-        // cy.get('#email').type(user_data.user_email)
-        register_page.fillEmail(user_data.user_email)
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
-    })
-
-    it('invalid e-mail', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#user').type(user_data.user_name)
-        register_page.fillName(user_data.user_name)
-        cy.get('#email').type('username@teste')
-        // cy.get('#password').type(user_data.user_password)
-        register_page.fillPassword(user_data.user_password)
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        // cy.get('#errorMessageFirstName')
-        //     .should('be.visible')
-        //     .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
-        register_page.checkMessage('O campo e-mail deve ser prenchido corretamente')
-    })
-
-    it('blank password field', () => {
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#user').type(user_data.user_name)
-        register_page.fillName(user_data.user_name)
-        // cy.get('#email').type(user_data.user_email)
-        register_page.fillEmail(user_data.user_email)
-        cy.get('#password').type('pass@')
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        // cy.get('#errorMessageFirstName')
-        //     .should('be.visible')
-        //     .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
-        register_page.checkMessage('O campo senha deve ter pelo menos 6 dígitos')
-    })
-
-    it('validating register modal successfully', () => {
-        // const name = faker.person.fullName()
-        // const email = faker.internet.email()
-
-        // cy.visit('/').get('.header-logo')
-        // cy.get('.fa-lock').click() //doubleClick //rightClick
-        // cy.get('#user').type(name)
-        register_page.fillName(user_data.user_name)
-        // cy.get('#email').type(email)
-        register_page.fillEmail(user_data.user_email)
-        // cy.get('#password').type(user_data.user_password)
-        register_page.fillPassword(user_data.user_password)
-        // cy.get('#btnRegister').click()
-        register_page.saveRegister()
-        // cy.get('#swal2-title')
-        //     .should('be.visible')
-        //     .should('have.text', 'Cadastro realizado!')
-        // cy.get('#swal2-html-container')
-        //     .should('be.visible')
-        //     .should('have.text', `Bem-vindo ${user_data.user_name}`)
-        register_page.checkModalMessage(user_data.user_name)
-        cy.get('.swal2-confirm')
-            .should('be.visible')
-            .should('have.text', 'OK')
-            .click()
-    });
 })
